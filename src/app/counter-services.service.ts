d68cb9095b084f94858ac70f874664fa2f663e59
iqbal-emon <ihemon281@gmail.com>
2024-02-10 21:47:34 +0600

    array

--- src/app/counter-services.service.ts
+++ src/app/counter-services.service.ts
@@ -4,13 +4,14 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class CounterServicesService {
-
   constructor() { }
+   countArray: number[] = [];
   counter:number=0;
   updateCounter(){
     this.counter++;
     console.log('count is',this.counter);
-
+    this.countArray.push(this.counter)
+console.log("count array",this.countArray);
 
   }
   getCounter(){
